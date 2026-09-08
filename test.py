0202654530610b1cb4b4d58e2d71b0b784329283
Gyakobo <andygyakobo@gmail.com>
2026-09-07 23:46:45 -0400

    Got the vocabulary size

---
+++ test.py
@@ -0,0 +1,6 @@
+with open("input.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
