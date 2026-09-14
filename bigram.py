365887457ca1a8e260469f770c36d1a1adbf6f52
Gyakobo <andygyakobo@gmail.com>
2026-09-14 02:15:00 -0400

    Implemented a multi-head

--- bigram.py
+++ bigram.py
@@ -107,13 +107,28 @@ class Head(nn.Module):
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    """
+    Multiple heads of self-attention in parallel
+    """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)  # Self attention head
+        self.sa_head = MultiHeadAttention(
+            4, n_embd // 4
+        )  # Self attention head(s), 4 heads of 8-dimensional self-attention
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
