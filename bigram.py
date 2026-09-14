c8ef2f2c129e23f14168be5d0e1413eb7b8d250c
Gyakobo <andygyakobo@gmail.com>
2026-09-14 02:19:54 -0400

    Applied the forward feed

--- bigram.py
+++ bigram.py
@@ -120,6 +120,22 @@ class MultiHeadAttention(nn.Module):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
 
+class FeedFoward(nn.Module):
+    """
+    a simple linear layer followed by a non-linearity
+    """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -129,6 +145,7 @@ class BigramLanguageModel(nn.Module):
         self.sa_head = MultiHeadAttention(
             4, n_embd // 4
         )  # Self attention head(s), 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedFoward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -146,6 +163,7 @@ class BigramLanguageModel(nn.Module):
 
         x = tok_emb + pos_emb  # (B, T, C) + (T, C)
         x = self.sa_head(x)  # apply one head of self-attention. (B, T, C)
+        x = self.ffwd(x)  # (B, T, C)
         logits = self.lm_head(x)  # (B, T, vocab_size)
 
         if targets is None:
